5f79b04c720f5ea2b6a5597a688c3552e5a614a4
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2019-10-16 12:17:32 +0200

    Get collection item title and subtitle and output format for Ads automation

--- scraper.py
+++ scraper.py
@@ -9,6 +9,22 @@ import time
 import json
 import urllib.parse
 
+
+# Function for getting collection item data
+def getItem(url, itemTitle, itemSubtitle):
+
+	# Load collection item
+	driver.get(url)
+	print("Item loaded: %s"%url)
+	time.sleep(0.5)
+
+	# Get title and subtitle of collection item
+	itemTitle = driver.find_element_by_css_selector("%s" % config['itemTitle']).get_attribute('innerHTML').replace('\n', '').replace('\t', '')
+	itemSubtitle = driver.find_element_by_css_selector("%s" % config['itemSubtitle']).get_attribute('innerHTML').replace('\n', '').replace('\t', '')
+	headingDict = {'title': itemTitle, 'subtitle': itemSubtitle}
+
+	return headingDict
+
 # Get configuration
 with open("config.json", 'r') as f:
 	config = json.load(f)
@@ -37,28 +53,43 @@ driver.get(url)
 # Return page source
 print("Page loaded: {}".format(driver.current_url))
 
-# Find themes
+# Scrape webpage
 time.sleep(2)
 themes = driver.find_elements_by_css_selector(config['theme'])
+print("Themes found")
 for theme in themes:
 	themeAttribute = theme.get_attribute(config['themeAttribute'])
 	if themeAttribute != 'home':
+
+		# Find themes
 		themeTitle = driver.find_element_by_css_selector("%s[%s='%s'] %s" % (config['theme'], config['themeAttribute'], theme.get_attribute(config['themeAttribute']),  config['themeTitle'])).get_attribute('innerHTML') 
-		themeDict = {'theme': themeTitle, 'subthemes': [], 'items': []}
+		themeDict = {'title': themeTitle, 'subtitle': '', 'link':'%s/#/%s'%(config['url'], themeAttribute), 'category': 'theme'}
+		sitemap.append(themeDict)
+
+		print("Theme %s appended"%themeTitle)
 
 		# Find subthemes
 		subthemes = driver.find_elements_by_css_selector("%s[%s='%s'] %s" % (config['theme'], config['themeAttribute'], theme.get_attribute(config['themeAttribute']),  config['subtheme']))
 		for subtheme in subthemes:
-			themeDict['subthemes'].append(subtheme.get_attribute(config['subthemeAttribute']))
+			subthemeTitle = driver.find_element_by_css_selector("%s[%s='%s'] %s" % (config['subtheme'], config['subthemeAttribute'], subtheme.get_attribute(config['subthemeAttribute']),  config['subthemeTitle'])).get_attribute('innerHTML') 
+			subthemeDict = {'title': subthemeTitle, 'subtitle': '', 'link':'%s/#/%s'%(config['url'], subtheme.get_attribute(config['subthemeAttribute'])), 'category': 'subtheme'}
+			sitemap.append(subthemeDict)
+
+			print("Subtheme %s appended"%subthemeTitle)
 
 		# Find collection items
 		items = driver.find_elements_by_css_selector("%s[%s='%s'] %s" % (config['theme'], config['themeAttribute'], theme.get_attribute(config['themeAttribute']),  config['item']))
 		for item in items:
-			themeDict['items'].append(item.get_attribute(config['itemAttribute']))
+			itemAttribute = item.get_attribute(config['itemAttribute'])
+			itemLink = '%s/#/%s'%(config['url'], itemAttribute)
 
-		sitemap.append(themeDict)
+			# Get item headings
+			itemHeadings = getItem(itemLink, config['itemTitle'], config['itemSubtitle'])
+			itemDict = {'title': itemHeadings['title'], 'subtitle': itemHeadings['subtitle'], 'link': itemLink, 'category': 'item'}
+			sitemap.append(itemDict)
 
-print("Themes found")
+			print("Item %s appended"%itemDict['title'])
+
 print(json.dumps(sitemap))
 
 #Exit program
